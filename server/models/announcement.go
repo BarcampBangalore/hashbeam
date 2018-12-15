214145061faa14d9a6e4737affb2818534a76408
tallpants <adithyakreddy6@gmail.com>
2018-12-16 00:34:15 +0530

    Reformat code

--- server/models/announcement.go
+++ server/models/announcement.go
@@ -6,13 +6,13 @@ import (
 )
 
 type Announcement struct {
-	ID string `bson:"_id"`
-	Time time.Time `bson:"time"`
-	Message string `bson:"message"`
+	ID      string    `bson:"_id"`
+	Time    time.Time `bson:"time"`
+	Message string    `bson:"message"`
 }
 
 func NewAnnouncement(message string) Announcement {
 	id := uuid.NewV4().String()
 	t := time.Now()
-	return Announcement{ id, t, message }
+	return Announcement{id, t, message}
 }
