def934085813adffcff39cd422112a59b5025287
tallpants <adithyakreddy6@gmail.com>
2018-12-23 01:02:57 +0530

    Formatter SQL

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,21 +1,27 @@
-CREATE TABLE IF NOT EXISTS `announcements` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `datetime` DATETIME NOT NULL,
-  `message` TEXT NOT NULL,
+CREATE TABLE IF NOT EXISTS `announcements`
+(
+  `id`       int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `datetime` DATETIME         NOT NULL,
+  `message`  TEXT             NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `tweets` (
-  `tweet_id` VARCHAR(45) NOT NULL,
-  `datetime` DATETIME NOT NULL,
-  `content` JSON NOT NULL,
-  `author_id` VARCHAR(45) NOT NULL,
-  `review_required` BOOLEAN NOT NULL,
+CREATE TABLE IF NOT EXISTS `tweets`
+(
+  `tweet_id`        VARCHAR(45) NOT NULL,
+  `datetime`        DATETIME    NOT NULL,
+  `content`         JSON        NOT NULL,
+  `author_id`       VARCHAR(45) NOT NULL,
+  `review_required` BOOLEAN     NOT NULL,
   PRIMARY KEY (`tweet_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `whitelisted_tweeters` (
+CREATE TABLE IF NOT EXISTS `whitelisted_tweeters`
+(
   `twitter_user_id` VARCHAR(45) NOT NULL,
-  `display_name` VARCHAR(45) NOT NULL,
+  `display_name`    VARCHAR(45) NOT NULL,
   PRIMARY KEY (`twitter_user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
