cbd9cc6008d027ae3e508c645f2c0bd174e8b282
tallpants <adithyakreddy6@gmail.com>
2018-12-23 01:02:30 +0530

    Schema for Twitter

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,7 +1,21 @@
 CREATE TABLE IF NOT EXISTS `announcements` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `datetime` datetime NOT NULL,
-  `message` text NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `datetime` DATETIME NOT NULL,
+  `message` TEXT NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE IF NOT EXISTS `tweets` (
+  `tweet_id` VARCHAR(45) NOT NULL,
+  `datetime` DATETIME NOT NULL,
+  `content` JSON NOT NULL,
+  `author_id` VARCHAR(45) NOT NULL,
+  `review_required` BOOLEAN NOT NULL,
+  PRIMARY KEY (`tweet_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `whitelisted_tweeters` (
+  `twitter_user_id` VARCHAR(45) NOT NULL,
+  `display_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`twitter_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
